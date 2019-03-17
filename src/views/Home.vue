921290764c4e5c5293775ad2931f011d30ce5105
Flamel001 <flamel001@yandex.ru>
2019-03-17 11:30:01 +0300

    My

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your js App" sign="1337"/>
   </div>
 </template>
 
---
+++ src/views/my.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>This is my page</h1>
+        <button @click="myButton()">myCoolButton</button>
+        <button @click="myButton2()">myCoolButton2</button>
+        <button @click="myButton3()">myCoolButton3</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'myz',
+    methods:{
+        myButton:()=>{
+            console.log(this);
+        },
+        myButton2: function(){
+            console.log(this);
+        },
+        myButton3(){
+            console.log(this);
+        }
+    }
+}
+</script>
