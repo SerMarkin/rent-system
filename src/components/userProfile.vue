1701564336834697b28c4cadeb5860ec57573971
Flamel001 <flamel001@yandex.ru>
2019-03-17 16:42:09 +0300

    3 profile tabs

---
+++ src/components/userProfile.vue
@@ -0,0 +1,35 @@
+<template>
+    <div style="text-align:left">
+        <h1>This is my profile page</h1>
+        <v-text-field v-model="name" label="Имя"></v-text-field>
+        <v-text-field v-model="surname" label="Фамилия"></v-text-field>
+        <v-text-field v-model="lastname" label="Отчество"></v-text-field>
+        <v-text-field v-model="email" label="Электронная почта" readonly></v-text-field>
+        <h2>Безопасность</h2>
+        <v-text-field v-model="oldPass" label="Старый пароль" type="password"></v-text-field>
+        <v-text-field v-model="newPass" label="Новый пароль" type="password"></v-text-field>
+        <v-btn @click="changePass">Изменить пароль</v-btn>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:"userProfile",
+    data(){
+        return{
+            name: "",
+            surname: "",
+            lastname: "",
+            email: "",
+            oldPass: "",
+            newPass: "",
+        }        
+    },
+    methods:{
+        changePass(){
+            
+        }
+    }
+}
+</script>
