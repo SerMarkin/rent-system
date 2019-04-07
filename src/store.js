e874f23fad14b4bab577c693cde46451c32c6fce
SergMark <kadil12353@gmail.com>
2019-04-07 21:21:07 +0300

    no docker

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
-    url: window.origin + '/api/',
+    url: 'https://inno-rent.herokuapp.com' + '/',
     user:{},
     auth:false
   },
